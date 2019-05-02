d40f62c459d414a3b8fdf943641df587144cc5f1
christypacc21 <christypacc21@gmail.com>
2019-05-02 23:19:42 +0800

    backend - connect mongodb(local) database

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const config = require("./DB.js");
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => {
+    console.log("Database is connected");
+  },
+  err => {
+    console.log("Can not connect to the database" + err);
+  }
+);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
