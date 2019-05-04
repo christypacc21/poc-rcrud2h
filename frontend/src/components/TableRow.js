f7af74e8fb46694949db52a101d2e3f04dffc2f6
christypacc21 <christypacc21@gmail.com>
2019-05-04 22:41:25 +0800

    (delete function) - refactor code

--- frontend/src/components/TableRow.js
+++ frontend/src/components/TableRow.js
@@ -1,37 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-class TableRow extends Component {
-  delete = () => {
+// Coz there is no state and no need a constructor, I change from using class component to functional compoennet
+const TableRow = data => {
+  const { personName, businessName, gstNum, _id } = data.items;
+
+  const Delete = () => {
     axios
-      .get(`http://localhost:5000/business/delete/${this.props.items._id}`)
+      .get(`http://localhost:5000/business/delete/${_id}`)
       .then(console.log("Deleted"))
       .catch(err => console.log(err));
 
     window.location.reload(); //temp method to refresh page
   };
 
-  render() {
-    const { personName, businessName, gstNum, _id } = this.props.items;
-    return (
-      <tr>
-        <td>{personName}</td>
-        <td>{businessName}</td>
-        <td>{gstNum}</td>
-        <td>
-          <Link to={`/edit/${_id}`} className="btn btn-primary">
-            Edit
-          </Link>
-        </td>
-        <td>
-          <button className="btn btn-danger" onClick={this.delete}>
-            Delete
-          </button>
-        </td>
-      </tr>
-    );
-  }
-}
+  return (
+    <tr>
+      <td>{personName}</td>
+      <td>{businessName}</td>
+      <td>{gstNum}</td>
+      <td>
+        <Link to={`/edit/${_id}`} className="btn btn-primary">
+          Edit
+        </Link>
+      </td>
+      <td>
+        <button className="btn btn-danger" onClick={Delete}>
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+};
 
 export default TableRow;
